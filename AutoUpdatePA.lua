c0bc5ed907ac2e6f09efafcfd3a1585aa8bd7062
CorbanYT <89632356+CorbanYT@users.noreply.github.com>
2023-08-08 16:44:17 +0300

    Update AutoUpdatePA.lua

--- AutoUpdatePA.lua
+++ AutoUpdatePA.lua
@@ -17,8 +17,8 @@ local function u8d(s) return encoding.UTF8:decode(s) end
 
 update_state = false
 
-local script_vers = 8
-local script_vers_text = "1.08"
+local script_vers = 7
+local script_vers_text = "1.07"
 
 local update_path = getWorkingDirectory() .. "/update.ini"
 local update_url = "https://raw.githubusercontent.com/CorbanYT/PatrolAssistant/main/update.ini"
@@ -26,12 +26,12 @@ local update_url = "https://raw.githubusercontent.com/CorbanYT/PatrolAssistant/m
 local script_path = thisScript().path
 local script_url = "https://github.com/CorbanYT/PatrolAssistant/raw/main/AutoUpdatePA.lua"
 
-
+    
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end
 	while not isSampAvailable() do wait(100) end
  
-    sampAddChatMessage(tag .. u8d"{FFFFFF}Скрипт по обновлению - {5A90CE}работает.", main_color)
+    sampAddChatMessage(tag .. u8d"{FFFFFF}������ �� ���������� - {5A90CE}��������.", main_color)
 
     sampRegisterChatCommand("update", cmd_update)
     
@@ -42,7 +42,7 @@ function main()
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
             if tonumber(updateIni.info.vers) > script_vers then 
-                sampAddChatMessage(tag .. u8d"{FFFFFF}Появилось новое обновление! Версия: {5A90CE}".. updateIni.info.vers_text, main_color)
+                sampAddChatMessage(tag .. u8d"{FFFFFF}��������� ����� ����������! ������: {5A90CE}".. updateIni.info.vers_text, main_color)
 		update_state = true
             end
             os.remove(update_path)
@@ -55,7 +55,7 @@ function main()
         if update_state then
             downloadUrlToFile(script_url, script_path, function(id, status) 
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage(tag .. u8d"{FFFFFF}Скрипт успешно {5A90CE}обновлен!", main_color)
+                    sampAddChatMessage(tag .. u8d"{FFFFFF}������ ������� {5A90CE}��������!", main_color)
                     thisScript():reload()
                 end
             end)
@@ -66,6 +66,6 @@ function main()
 end
 
 function cmd_update(arg)
-    sampShowDialog(1000, u8d"Автообновление новое!!!!", u8d"Это автообновление детка", u8d"Закрыть", "", 0)
-    sampAddChatMessage(u8d"{FFFFFF}теставатватва {5A90CE}обновлен!", main_color)
+    sampShowDialog(1000, u8d"�������������� �����!!!!", u8d"��� �������������� �����", u8d"�������", "", 0)
+    sampAddChatMessage(u8d"{FFFFFF}������������� {5A90CE}��������!", main_color)
 end
